7e42b605a041e709ed94095bff79748be8544ee6
GlennJohansson85 <g.j@live.se>
2024-11-29 02:24:21 +0100

    collect static

--- project_1/settings.py
+++ project_1/settings.py
@@ -97,7 +97,7 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
-    }
+   }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
